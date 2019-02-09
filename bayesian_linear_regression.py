1474429e0487f6577d79d90d6749c582f581a49b
adesam146 <a.samuel.o@hotmail.co.uk>
2019-02-09 12:06:23 +0000

    Cteated Data Loader for training set

--- bayesian_linear_regression.py
+++ bayesian_linear_regression.py
@@ -1,6 +1,8 @@
 import math
+import numpy as np
 import torch
 import torch.distributions as trd
+from torch.utils.data import TensorDataset, DataLoader
 # import tensorflow as tf
 # import tensorflow_probability as tfp
 # tfd = tfp.distributions
@@ -21,7 +23,7 @@ def linear_dataset(beta_true, N, noise_std=0.1):
     For now just assuming that beta_true is a scalar
     """
     X = np.random.normal(loc=0, scale=1, size=(N, 1))
-    noise = np.random.normal(loc=0, stddev=noise_std, size=(N, 1))
+    noise = np.random.normal(loc=0, scale=noise_std, size=(N, 1))
 
     Y = beta_true * X + noise
 
@@ -37,7 +39,7 @@ class TrainableNormal:
     def log_prob(self, x):
         sigma2 = math.exp(2 * self.ln_sigma)
 
-        return -0.5 * math.log(2*math.pi) - 0.5 * math.log(sigma2) - 0.5 * (1/sigma2) * (x - self.init_mean)**2
+        return -0.5 * math.log(2*math.pi) - 0.5 * math.log(sigma2) - 0.5 * (1/sigma2) * (x - self.mean)**2
 
     def sample(self, n=1):
         with torch.no_grad():
@@ -97,3 +99,11 @@ def inference(prior, approx_posterior, data_loader, model_simulator, approx_simu
             posterior_optimizer.step()
 
         print("Epoch: ", epoch, "ratio_loss:", ratio_loss, "posterior_loss:", posterior_loss)
+
+beta_true=10
+N_train = 1000
+
+X_train, Y_train = linear_dataset(beta_true, N_train)
+data_loader_train = DataLoader(TensorDataset(X_train, Y_train), batch_size=1)
+
+inference(prior, posterior, data_loader_train, None, None)
