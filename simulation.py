d31ade16d0eba7c59e1d1824f13faa23e53e4742
adesam146 <a.samuel.o@hotmail.co.uk>
2019-04-02 18:04:14 +0100

    Auto formatting

--- simulation.py
+++ simulation.py
@@ -9,21 +9,23 @@ if torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
 torch.manual_seed(0)
 
+
 def get_expert_trajectories(N, end=100, T=10, std_div=1):
-  """
-  """
-  loc = torch.linspace(start=0, end=end, steps=T)
-  cov = torch.diag(std_div**2 * torch.ones_like(loc))
+    """
+    """
+    loc = torch.linspace(start=0, end=end, steps=T)
+    cov = torch.diag(std_div**2 * torch.ones_like(loc))
 
-  distn = trd.MultivariateNormal(loc, covariance_matrix=cov)
+    distn = trd.MultivariateNormal(loc, covariance_matrix=cov)
 
-  return distn.sample((N,))
+    return distn.sample((N,))
+
 
 fig, ax = plt.subplots()
 
 trajectories = get_expert_trajectories(10)
 for t in trajectories:
-  t_ = t.numpy()
-  ax.scatter(t_, np.zeros_like(t_))
+    t_ = t.numpy()
+    ax.scatter(t_, np.zeros_like(t_))
 
 plt.show()
