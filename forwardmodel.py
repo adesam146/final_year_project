85ec325de8d0d6b9c66066070b5a2b3f6cec4ec5
adesam146 <a.samuel.o@hotmail.co.uk>
2019-05-08 22:15:12 +0100

    Fixed forgetting to add update to forwardmodel

--- forwardmodel.py
+++ forwardmodel.py
@@ -11,15 +11,18 @@ import matplotlib.colorbar as cbar
 
 
 class ForwardModel:
-    def __init__(self, init_x, init_y, device):
+    def __init__(self, init_x, init_y, D, S, F, device):
         """
         This model assumes the states and actions have already been paired
         to form the input into the GP
-        init_x: N x D+F
+        init_x: N x S+F
         init_y: N x D
         """
-        self.D = init_y.shape[1]
-        self.F = init_x.shape[-1] - self.D
+        assert init_x.shape[-1] == S+F
+        assert init_y.shape[-1] == D
+        self.D = D
+        self.S = S
+        self.F = F
         self.device = device
 
         self.train_x, self.train_y = self.__process_inputs(init_x, init_y)
@@ -31,21 +34,25 @@ class ForwardModel:
 
     def predict(self, x):
         """
-        x: Tst x D+F
-        output: Tst x D
+        x: 1 x S+F
+        sample: 1 x D
+        log_prob: 1
         """
         assert self.model is not None
+        assert x.shape[-1] == self.S + self.F
 
         # A model evaluated at x just returns a pytorch multivariate gaussian and calling the likelihood just transforms the distribution apporiately, i.e. at the noise variance
         # Output from sample is (n x ) D x Tst = (1 x) 1 x 1. Where n is number of sample and Tst is number of test points
-        output = torch.t(self.likelihood(self.model(
-            self.__adjust_shape(x))).rsample())
+        dyn_model = self.likelihood(self.model(self.__adjust_shape(x)))
 
-        return output
+        sample = dyn_model.sample()
+        log_probs = dyn_model.log_prob(sample)
+
+        return torch.t(sample), log_probs.sum() 
 
     def __mean(self, x):
         """
-        x: (Tst) x D+F
+        x: (Tst) x S+F
         output: D
         """
         self.model.eval()
@@ -103,11 +110,11 @@ class ForwardModel:
 
         # Random computation for force the kernel to be evaluated
         # The reason this is done is that you do not want the first time the kernel for the data is calculated to be in a loop and involve a tensor that requires_grad because onces backward is called, in a subsequence iteration due to gpytorch's lazy loading, autograd would try to access the TODO
-        self.predict(torch.zeros(1, self.train_x.shape[-1], device=self.device))
+        self.predict(torch.zeros(1, self.S+self.F, device=self.device))
 
     def update_data(self, new_x, new_y):
         """
-        new_x: N x D+F
+        new_x: N x S+F
         new_y: N x D
         """
         new_x, new_y = self.__process_inputs(new_x, new_y)
@@ -117,37 +124,43 @@ class ForwardModel:
 
     def add_fantasy_data(self, x, y):
         """
-        x: (N x) D+F
+        x: (N x) S+F
         y: (N x) D
         """
         assert self.model is not None
+        assert x.shape[-1] == self.S+self.F
+        assert y.shape[-1] == self.D
 
-        x, y = self.__process_inputs(x.view(-1, self.D+self.F), y.view(-1, self.D))
+        x, y = self.__process_inputs(x.view(-1, self.S+self.F), y.view(-1, self.D))
 
         self.dummy_x = torch.cat((self.dummy_x, x), dim=1)
         self.dummy_y = torch.cat((self.dummy_y, y), dim=1)
 
         self.model.set_train_data(self.dummy_x, self.dummy_y, strict=False)
 
+    def clear_fantasy_data(self):
+        self.dummy_x = self.train_x
+        self.dummy_y = self.train_y
+
     def __process_inputs(self, x, y):
         """
         Puts inputs in batch form for the Batch GP
-        x: N x D+F
+        x: N x S+F
         y: N x D
-        output[0]: D x N x D+F
+        output[0]: D x N x S+F
         output[1]: D x N
         """
-        # X is now D x N x D+F which is what the GP expects
         x = self.__adjust_shape(x)
-        # y is now D x N
+        # x is now D x N x S+F which is what the GP expects
         y = torch.t(y)
+        # y is now D x N
 
         return x, y
 
     def __adjust_shape(self, x):
         """
-        x: Tst x D+F
-        output: D x Tst x D+F
+        x: Tst x S+F
+        output: D x Tst x S+F
         Need to replicate x for each target dimension
         """
         return x.unsqueeze(0).repeat(self.D, 1, 1)
@@ -194,10 +207,10 @@ class GPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         """
         Let D be the dimension of the functions output,
-        D+F the dimension of the input and N the number of data.
+        S+F the dimension of the input and N the number of data.
         We would model the actual function with D separate/independant GPs 
         train_y: D x N
-        train_x: D x N x D+F
+        train_x: D x N x S+F
         """
         super(GPModel, self).__init__(train_x, train_y, likelihood)
         num_output = train_y.shape[0]
