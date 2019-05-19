633dc23616e0f95e631f2ed0bb44f5247454abd8
adesam146 <a.samuel.o@hotmail.co.uk>
2019-05-19 20:51:13 +0100

    Added some noise to GP training targets

--- cartpole/utils.py
+++ cartpole/utils.py
@@ -36,7 +36,7 @@ def get_training_data(s_a_pairs, traj):
         s_a_pairs[:, :D], D), s_a_pairs[:, D:]), dim=1)
 
     y = traj[1:] - traj[:-1]
-    return x, y
+    return x, y + 0.01 * torch.randn_like(y)
 
 
 def convert_to_aux_state(state, D):
