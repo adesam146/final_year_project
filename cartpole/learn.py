2a2de18fec57f23e2cdd1530f0681e7bab65631e
adesam146 <a.samuel.o@hotmail.co.uk>
2019-05-20 19:18:48 +0100

    Reduced policy opti iter to 50

--- cartpole/learn.py
+++ cartpole/learn.py
@@ -87,7 +87,7 @@ with gpytorch.settings.fast_computations(covar_root_decomposition=False, log_pro
 
         policy_lr_sch.step()
 
-        for i in range(100):
+        for i in range(50):
             # Optimize policy for given forward model
 
             def get_samples_and_log_prob():
