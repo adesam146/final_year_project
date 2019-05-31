0de8fde17a508cc0157a0043e904075129099ad0
adesam146 <a.samuel.o@hotmail.co.uk>
2019-05-31 12:32:33 +0100

    Added flags for each policy optimisation algo

--- cartpole/learn.py
+++ cartpole/learn.py
@@ -38,6 +38,10 @@ parser.add_argument("--description", help="Description the experiment")
 parser.add_argument("--T", type=int, help="Number of predicted timesteps")
 parser.add_argument("--use_score_func_grad",
                     help="Use score function gradient method for optimising policy", action="store_true")
+parser.add_argument("--use_pathwise_grad",
+                    help="Use pathwise gradient method for optimising policy", action="store_true")
+parser.add_argument("--use_max_log_prob",
+                    help="Use maxising the log prob of forward method for optimising policy", action="store_true")
 args = parser.parse_args()
 
 # *** RESULTS LOGGING SETUP ***
@@ -79,14 +83,24 @@ if GPU:
 else:
     device = torch.device("cpu")
 
+# *** POLICY OPTIMISATION ALGORITHM CHOICE ***
+use_score_func_grad = args.use_score_func_grad
+use_pathwise_grad = args.use_pathwise_grad
+use_max_log_prob = args.use_max_log_prob
+
+# *** GET EXPERT SAMPLES ***
 expert_samples = get_expert_data().to(device)
 
 # N is the number of samples from each source (expert/predicted) to be compared against each other
 setup = CartPoleSetup(
     N=expert_samples.shape[0], T=args.T or expert_samples.shape[1]-1)
 
+# Determine whether expert x0 is needed or not
+expert_sample_start = 0
+if not use_max_log_prob:
+    expert_sample_start = 1
 # Restrict samples to specfied horizon T
-expert_samples = expert_samples[:, 1:setup.T+1, :]
+expert_samples = expert_samples[:, expert_sample_start:setup.T+1, :]
 
 # *** POLICY SETUP ***
 # policy = RBFPolicy(u_max=10, input_dim=setup.aux_state_dim, nbasis=10, device=device)
@@ -127,7 +141,6 @@ disc_optimizer = torch.optim.Adam(disc.parameters())
 
 num_of_experience = 50
 policy_iter = args.policy_iter or 50
-use_score_func_grad = args.use_score_func_grad
 
 # Write to a json file all defined variables before training starts
 with open(variables_file, 'w') as fp:
@@ -142,34 +155,43 @@ with gpytorch.settings.fast_computations(covar_root_decomposition=False, log_pro
         policy_lr_sch.step()
 
         for i in range(policy_iter):
-            if use_score_func_grad:
-                disc_loss, policy_loss, samples, actions = score_function_training(
-                    setup, N_x0, expert_samples, policy, fm, disc, disc_optimizer, policy_optimizer, init_state_distn)
-            else:
-                disc_loss, policy_loss, samples, actions = pathwise_grad(
-                    setup, expert_samples, policy, fm, disc, disc_optimizer, policy_optimizer, init_state_distn)
+            if use_score_func_grad or use_pathwise_grad:
+                if use_score_func_grad:
+                    disc_loss, policy_loss, samples, actions = score_function_training(
+                        setup, N_x0, expert_samples, policy, fm, disc, disc_optimizer, policy_optimizer, init_state_distn)
+                else:
+                    disc_loss, policy_loss, samples, actions = pathwise_grad(
+                        setup, expert_samples, policy, fm, disc, disc_optimizer, policy_optimizer, init_state_distn)
 
-            if i % 10 == 0:
-                plot_gp_trajectories(
-                    samples, actions, T=setup.T, plot_dir=plot_dir, title=f'Predicted trajectories with {expr} interactions and at {i} policy iterations.', file_name=f'training_trajs_expr-{expr}-policy_iter-{i}')
+                if i % 10 == 0:
+                    plot_gp_trajectories(
+                        samples, actions, T=setup.T, plot_dir=plot_dir, title=f'Predicted trajectories with {expr} interactions and at {i} policy iterations (before the {i+1}th parameter update).', file_name=f'training_trajs_expr-{expr}-policy_iter-{i}')
 
-            del samples, actions
+                del samples, actions
 
-            # policy_optimizer.zero_grad()
-            # loss = 0
-            # for t in range(setup.T):
-            #     for n in range(setup.N):
-            #         # TODO: Do we need to include the likelihood how that we
-            #         # are looking at the observed difference
-            #         aux_x = convert_to_aux_state(expert_samples[n, t], setup.state_dim)
-            #         dyn_model = fm.predictive_distn(torch.cat( (aux_x, policy(aux_x).view(-1, setup.action_dim)), dim=1))
-            #         loss += -dyn_model.log_prob((expert_samples[n, t+1] - expert_samples[n, t]).view(setup.state_dim, -1)).sum()
+                print(
+                    f"Experience {expr}, Iter {i}, disc loss: {disc_loss.detach().item()}, policy loss: {policy_loss.detach()}")
 
-            # policy_optimizer.step()
-            # print(f"Experience {expr}, Iter {i}, policy loss: {loss.detach().item()}")
+            if use_max_log_prob:
+                policy_optimizer.zero_grad()
+                loss = 0
+                for n in range(setup.N):
+                    for t in range(setup.T):
+                        # TODO: Do we need to include the likelihood how that we
+                        # are looking at the observed difference
+                        # This can perhaps also be done in a batch form
+                        aux_x = convert_to_aux_state(
+                            expert_samples[n, t], setup.state_dim)
+                        dyn_model = fm.predictive_distn(
+                            torch.cat((aux_x, policy(aux_x).view(-1, setup.action_dim)), dim=1))
+                        loss += -dyn_model.log_prob(
+                            (expert_samples[n, t+1] - expert_samples[n, t]).view(setup.state_dim, -1)).sum()
 
-            print(
-                f"Experience {expr}, Iter {i}, disc loss: {disc_loss.detach().item()}, policy loss: {policy_loss.detach()}")
+                loss *= 1.0/setup.N
+                loss.backward()
+                policy_optimizer.step()
+                print(
+                    f"Experience {expr}, Iter {i}, policy loss: {loss.detach().item()}")
 
         policy.eval()
         # Get more experienial data
